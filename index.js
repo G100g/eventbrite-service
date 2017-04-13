d7348ba39982930a94df98847f100da35ad6a9e9
Giorgio Aquino <giorgio.aquino@ynap.com>
2017-04-13 15:05:00 +0200

    Add partecipants

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const { send } = require('micro');
-var Nbrite = require('nbrite');
+const Nbrite = require('nbrite');
+
+const url = require('url') ;
+
+const EVENTBRITE_ACCESS_TOKEN = getToken();
+const nbrite = new Nbrite({token: EVENTBRITE_ACCESS_TOKEN });
 
 function getToken() {
     return process.env.EB_TOKEN;
@@ -8,24 +13,21 @@ function getToken() {
 function reducePayload(items, event) {
 
     items.push({
+        id: parseInt(event.id),
         name: event.name,
         start: event.start,
         end: event.end,
         url: event.url,
         logo: event.logo,
         description: event.description,
+        capacity: event.capacity,
     });
 
     return items;
 
 }
 
-function eventbrite() {
-
-  const EVENTBRITE_ACCESS_TOKEN = getToken();
-
-  var nbrite = new Nbrite({token: EVENTBRITE_ACCESS_TOKEN });
-
+function events() {
   return nbrite.get('/users/me/owned_events', { status: 'live' }).then(function (res) {
       if (res.events) {
         return res.events.reduce(reducePayload, []);
@@ -33,14 +35,34 @@ function eventbrite() {
 
       return {};
   });
+}
 
+function attendees(id) {
+  return nbrite.get(`/events/${id}/attendees/`, { status: 'attending' }).then(function (res) {
+      if (res.pagination) {
+        return {
+            partecipants: res.pagination.object_count
+        };
+      }
+
+      return {};
+  });
 }
 
 const handler = async (req, res) => {
-    const events = await eventbrite();
+    
+    var queryObject = url.parse(req.url,true).query;
+    let response = {};
+
+    if (queryObject.eventid) {
+        response = await attendees(queryObject.eventid);
+    } else {
+        response = await events();
+    }
+
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader("Content-Type", "application/json; charset=utf-8");
-    send(res, 200, events);
+    send(res, 200, response);
 };
 
 module.exports = handler;
